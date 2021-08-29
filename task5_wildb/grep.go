c7e515ce3ff2f58233325d83f1f6b34e07b89ed8
Waltyr Adina <wadina@oa-j1.msk.21-school.ru>
2021-08-29 20:06:27 +0300

    5

--- task5_wildb/grep.go
+++ task5_wildb/grep.go
@@ -53,31 +53,34 @@ func parseFlags() argFlag {
 			}
 			if strings.Contains(os.Args[i], "A") {
 				ind := strings.Index(os.Args[i], "A")
-				if isDigit(rune(os.Args[i][ind + 1])) {
-					flags.A, _ = strconv.Atoi(os.Args[i][2:])
-				} else {
+				if len(os.Args[i]) < 3 || !isDigit(rune(os.Args[i][ind + 1])) {
 					err := fmt.Errorf("UNKNOW FLAG")
 					fmt.Println(err)
+					
+				} else {
+					flags.A, _ = strconv.Atoi(os.Args[i][2:])
 				}
 				flags.i = true
 			}
 			if strings.Contains(os.Args[i], "B") {
 				ind := strings.Index(os.Args[i], "B")
-				if isDigit(rune(os.Args[i][ind + 1])) {
-					flags.B, _ = strconv.Atoi(os.Args[i][2:])
-				} else {
+				if len(os.Args[i]) < 3 || !isDigit(rune(os.Args[i][ind + 1])) {
 					err := fmt.Errorf("UNKNOW FLAG")
 					fmt.Println(err)
+					
+				} else {
+					flags.B, _ = strconv.Atoi(os.Args[i][2:])
 				}
 				flags.i = true
 			}
 			if strings.Contains(os.Args[i], "C") {
 				ind := strings.Index(os.Args[i], "C")
-				if isDigit(rune(os.Args[i][ind + 1])) {
-					flags.C, _ = strconv.Atoi(os.Args[i][2:])
-				} else {
+				if len(os.Args[i]) < 3 || !isDigit(rune(os.Args[i][ind + 1])) {
 					err := fmt.Errorf("UNKNOW FLAG")
 					fmt.Println(err)
+					
+				} else {
+					flags.C, _ = strconv.Atoi(os.Args[i][2:])
 				}
 				flags.i = true
 			}
@@ -93,7 +96,6 @@ func parseFlags() argFlag {
 		err := fmt.Errorf("Нет аргументов")
 		fmt.Println(err)
 	}
-	fmt.Println(len(os.Args))
 	return flags
 }
 
@@ -150,12 +152,49 @@ func createArr(arg argFlag) [][]string{
 	return strs
 }
 
-func outputStrings(strs0 [][]string, arg argFlag) {
-	strs := make([][]string, 0, 0)
-	strs = append(strs, strs0...)
+func arrarrContain(ss []string, arg argFlag) bool {
+	for i := range ss {
+		if strings.Contains(ss[i], arg.word) {
+			return true
+		}
+	}
+	return false
+}
+
+func reArray(ss0 [][]string, arg argFlag) [][]string {
+	ss := make([][]string, 0, 0)
+	ss2 := make([][]string, 0, 0)
 	
-	fmt.Printf("%p %p\n", strs, strs0)
-	
+	ss = append(ss, ss0...)
+	for i := range ss {
+		match := arrarrContain(ss[i], arg)
+		if match {
+			// if len(ss2) != 0 && ss2[len(ss2) - 1] {
+
+			// }
+			ss2 = append(ss2, ss[i])
+			match2 := false
+			for k := 1; !match2 && k <= arg.A && k + i < len(ss); k++ {
+				match2 = false
+				for l := 1; l < len(ss[i + k]); l++ {
+					if strings.Contains(ss[i + k][l], arg.word) {
+						match2 = true
+						break
+					}
+				}
+				if !match2 {
+					ss2 = append(ss2, ss[i + k])
+				}
+			}
+		}
+		
+	}
+	return ss2
+}
+
+func outputStrings(strs [][]string, arg argFlag) {
+		
+	fmt.Println("len(strs)", len(strs))
 	for i := range strs {
 		for j := range strs[i] {
 			if j != 0 {
@@ -167,9 +206,15 @@ func outputStrings(strs0 [][]string, arg argFlag) {
 }
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("некорретные входные данные\n КАК?: grep [-civFn] [-A num] [-B num] [-C[num] [filter key] [file ...]\n-A - `after` печатать +N строк после совпадения\n-B - `before` печатать +N строк до совпадения\n-C - `context` (A+B) печатать ±N строк вокруг совпадения\n-c - `count` (количество строк)\n-i - `ignore-case` (игнорировать регистр)\n-v - `invert` (вместо совпадения, исключать)\n-F - `fixed`, точное совпадение со строкой, не паттерн\n-n - `line num`, печатать номер строки")
+		return
+	}
 	arg := parseFlags()
-	strs := createArr(arg)
-	outputStrings(strs, arg)
 	fmt.Println(arg)
-	// outputStrings(strs, arg)
+	strs := createArr(arg)
+	strs2 := reArray(strs, arg)
+	
+	outputStrings(strs, arg)
+	outputStrings(strs2, arg)
 }
