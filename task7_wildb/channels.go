d35493cae2acfbf6cdfc3361bf3c08fc455161a8
Mel.Aleksandr <mel.aleksandr@wildberries.ru>
2021-09-08 12:53:34 +0300

    7

--- task7_wildb/channels.go
+++ task7_wildb/channels.go
@@ -4,18 +4,34 @@ import (
 	"fmt"
 	"time"
 )
+//or сливает каналы в 1 до получения сигнала по одному из них
+func or(channels ...<- chan interface{}) <- chan interface{} {
+	out := make(chan interface{})
+	stop := make(chan interface{}, len(channels))
+	done := make(chan interface{})
+	
+	for _, ch := range channels {
+    	go func(ch <- chan interface{}) {
+			for {
+				var t interface{}
+				select {
+				case <-stop :
+					return
+				case t = <-ch :
+					done<- t
+					out <- t
+				default :
+					continue
+				}
+			}
+		}(ch)
+	}
+	<-done
+	close(stop)
+    return out
+}
 
 func main() {
-	var or func(channels ...<- chan interface{})
-	or = func(channels ...<- chan interface{}) {
-		for i, ch := range channels {
-			fmt.Printf("tipe of channels[%d]=%T\n", i, <-ch)
-			
-		}
-		
-	}
-
-
     sig := func(after time.Duration) <- chan interface{} {
         c := make(chan interface{})
         go func() {
@@ -24,16 +40,19 @@ func main() {
     	}()
     	return c
 	}
-
 	start := time.Now()
-	or (
-		
-		sig(1*time.Second),
-		sig(2*time.Second),
+	<- or (	
 		sig(5*time.Second),
+		sig(4*time.Second),
 		sig(3*time.Second),
+		sig(2*time.Second),
+		sig(6*time.Second),
+		sig(1*time.Second),
+		sig(1*time.Second),
+		sig(1*time.Second),
+		sig(1*time.Second),
+		sig(1*time.Second),
 	)
-
 	fmt.Printf("fone after %v", time.Since(start))
 	fmt.Println(1)
 
