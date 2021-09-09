f7e1a03b54aeb5c7f95ed1f805823cf5f6072b69
Mel.Aleksandr <mel.aleksandr@wildberries.ru>
2021-09-09 14:08:13 +0300

    8

---
+++ task8_wildb/shell.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        // Read the keyboad input.
+        input, err := reader.ReadString('\n')
+        if err != nil {
+            fmt.Fprintln(os.Stderr, err)
+        }
+		fmt.Println(input)
+    }
+}
