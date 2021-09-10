7d46504edfef0780805262c878153e298b2a6c03
Mel.Aleksandr <mel.aleksandr@wildberries.ru>
2021-09-10 16:31:45 +0300

    8

--- task8_wildb/shell.go
+++ task8_wildb/shell.go
@@ -6,15 +6,42 @@ import (
 	"os"
 )
 
+type Command struct {
+    Name string
+}
+
+type Status struct {
+    Dir string
+
+}
+
+func parseDir(com Command) {
+    
+}
+
+func parseCmd(input string) {
+    if input == "ls" {
+        var cmd Command
+        cmd.Name = "ls"
+    }
+}
+
 func main() {
-	
+    var stat Status
     reader := bufio.NewReader(os.Stdin)
     for {
-        // Read the keyboad input.
+        
+        
         input, err := reader.ReadString('\n')
         if err != nil {
             fmt.Fprintln(os.Stderr, err)
+            break
         }
-		fmt.Println(input)
+
+        com := parseCmd(input)
+        parseStatus(stat)
+
+		fmt.Printf("input=%v, err=%v T=%T\n", input, err, err)
+        
     }
 }
